c0f9f6183371c4221e61563c99ccdfc8ef664210
AmierohAbrahams <3559244@myuwc.ac.za>
2019-11-24 19:04:34 +0200

    Things to do...

--- SAWS_extracting.R
+++ SAWS_extracting.R
@@ -110,12 +110,6 @@ Upwelling_index <- wind_daily %>%  # Making refeerence to the wind daily datta c
 # UI dataset produces the upwelling index
 write_csv(Upwelling_index, path = "Upwelling_index.csv")
 
-
-
-upwelling_graph <- wind.data %>% 
-  summarise(mean_temp = mean(temp),
-            sd_temp = sd(temp))
-
 # Things to change:
   # Only get for the west coast site
   # Match the names for each of the sites to the UI
